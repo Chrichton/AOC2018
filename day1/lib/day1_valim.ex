80837e3ecbaf787f3ab5dbf7d782920cbaeebee6
Heiko Goes <heiko.goes@gaebler-goes.de>
2021-11-28 05:40:09 +0100

    Use Proces-Dictionary

--- day1/lib/day1_valim.ex
+++ day1/lib/day1_valim.ex
@@ -12,17 +12,30 @@ defmodule Day1Valim do
   end
 
   defp repeated_frequency(file_stream) do
-    file_stream
-    |> Stream.cycle()
-    |> Enum.reduce_while({0, MapSet.new([0])}, fn x, {current_frequency, seen_frequencies} ->
-      new_frequency = current_frequency + x
+    # use Task, so that the Process-Dictionary is not leaking.
+    # Otherwise it would be global
+    # time HashSet: 0.1 sec
+    # time Process: 1,27s user 0,34s system 147% cpu 1,094 total
+    # On Valims Machine, this is faster than HashSet (why?)
 
-      if new_frequency in seen_frequencies do
-        {:halt, new_frequency}
-      else
-        {:cont, {new_frequency, MapSet.put(seen_frequencies, new_frequency)}}
-      end
+    Task.async(fn ->
+      Process.put({__MODULE__, 0}, true)
+
+      file_stream
+      |> Stream.cycle()
+      |> Enum.reduce_while(0, fn x, current_frequency ->
+        new_frequency = current_frequency + x
+        key = {__MODULE__, new_frequency}
+
+        if Process.get(key) do
+          {:halt, new_frequency}
+        else
+          Process.put(key, true)
+          {:cont, new_frequency}
+        end
+      end)
     end)
+    |> Task.await(:infinity)
   end
 
   defp read_frequency_changes(filename) do
