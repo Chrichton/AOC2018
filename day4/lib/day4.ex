5dc572b689878cf23acc232ee30f96c1bfa1a618
Heiko Goes <hgoes@aidminutes.com>
2021-12-26 12:44:18 +0100

    test "parse_minutes"

--- day4/lib/day4.ex
+++ day4/lib/day4.ex
@@ -1,4 +1,6 @@
 defmodule Day4 do
+  use Timex
+
   def solve1(filename) do
   filename
     |> read_input()
@@ -11,6 +13,42 @@ defmodule Day4 do
 
   def read_input(filename) do
     File.read!(filename)
+    filename
     |> String.split("\n", trim: true)
+    |> Enum.reduce({Map.new(), 0}, fn line, {map, guard_id} ->
+      if String.contains?(line, "Guard #") do
+        guard_id = parse_guard_lines(line)
+        {map, guard_id}
+      else
+         minutes = parse_minutes(line)
+         Map.update(map, guard_id, [minutes], fn minutes_list ->
+          [minutes | minutes_list] end)
+      end
+    end)
+  end
+
+  def parse_guard_lines(lines) do
+   lines
+    |> Enum.take(1)
+    |> then(fn [guard_id_line] ->
+      String.split(guard_id_line, "#")
+      |> Enum.at(1)
+      |> String.split(" ", trim: true)
+      |> Enum.at(0)
+      |> String.to_integer()
+    end)
+  end
+
+  def parse_minutes(line) do
+    line
+    |> String.split("]", trim: true)
+    |> Enum.at(0)
+    |> then(fn line ->
+      line
+      |> String.slice(1, String.length(line) - 1)
+      |> then(fn date_string -> date_string <> ":00"
+      end)
+      |> NaiveDateTime.from_iso8601!()
+    end)
   end
 end
--- day4/test/day4_test.exs
+++ day4/test/day4_test.exs
@@ -1,6 +1,12 @@
 defmodule Day4Test do
   use ExUnit.Case
 
+  test "parse_minutes" do
+    actual = Day4.parse_minutes("[1518-11-01 00:05] falls asleep")
+
+    assert actual == ~N[1518-11-01 00:05:00]
+  end
+
   test "sample1" do
     assert Day4.solve1("sample1") == 4
   end
