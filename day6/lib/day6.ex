108dc687716037c1ce39ef59158796b0cc7baf2e
Heiko Goes <heiko.goes@gaebler-goes.de>
2022-01-03 19:27:01 +0100

    read_input + inner_points

--- day6/lib/day6.ex
+++ day6/lib/day6.ex
@@ -1,7 +1,45 @@
 defmodule Day6 do
+  import NimbleParsec
+
   def solve1(filename) do
     filename
     |> read_input()
+  end
+
+  def parse_coordinate(string) do
+    {:ok, [x, y], "", _, _, _} = parsec_coordinate(string)
+
+    {x, y}
+  end
+
+  def manhattan_distance({x1, y1}, {x2, y2}), do: abs(x1 - x2) - abs(y1 - y2)
+
+  @spec inner_points([{integer(), integer()}]) :: [{integer(), integer()}]
+  def inner_points(points) do
+    min_x =
+      points
+      |> Enum.min_by(fn {x, _y} -> x end)
+      |> elem(0)
+
+    max_x =
+      points
+      |> Enum.max_by(fn {x, _y} -> x end)
+      |> elem(0)
+
+    min_y =
+      points
+      |> Enum.min_by(fn {_x, y} -> y end)
+      |> elem(1)
+
+    max_y =
+      points
+      |> Enum.max_by(fn {_x, y} -> y end)
+      |> elem(1)
+
+    for x <- min_x..max_x,
+        y <- min_y..max_y do
+      {x, y}
+    end
   end
 
   def solve2(filename) do
@@ -11,5 +49,16 @@ defmodule Day6 do
 
   def read_input(filename) do
     File.read!(filename)
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_coordinate/1)
   end
+
+  # Parsing ----------------------------------------------------------------
+
+  defparsecp(
+    :parsec_coordinate,
+    integer(min: 1)
+    |> ignore(string(", "))
+    |> integer(min: 1)
+  )
 end
--- day6/test/day6_test.exs
+++ day6/test/day6_test.exs
@@ -1,6 +1,22 @@
 defmodule Day6Test do
   use ExUnit.Case
 
+  test "parse_coordinate" do
+    assert Day6.parse_coordinate("1, 6") == {1, 6}
+  end
+
+  test "read_input" do
+    actual = Day6.read_input("sample1")
+
+    assert actual == [{1, 1}, {1, 6}, {8, 3}, {3, 4}, {5, 5}, {8, 9}]
+  end
+
+  test "inner_points" do
+    actual = Day6.inner_points([{3, 4}, {1, 2}])
+
+    assert actual == [{1, 2}, {1, 3}, {1, 4}, {2, 2}, {2, 3}, {2, 4}, {3, 2}, {3, 3}, {3, 4}]
+  end
+
   test "sample1" do
     assert Day6.solve1("sample1") == 10
   end
