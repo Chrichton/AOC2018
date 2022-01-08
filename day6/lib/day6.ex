499edc5dfd5ba6312ca98a79df12999015429582
Heiko Goes <heiko.goes@gaebler-goes.de>
2022-01-08 08:09:24 +0100

    create_islands

--- day6/lib/day6.ex
+++ day6/lib/day6.ex
@@ -12,9 +12,6 @@ defmodule Day6 do
     {x, y}
   end
 
-  def manhattan_distance({x1, y1}, {x2, y2}), do: abs(x1 - x2) - abs(y1 - y2)
-
-  @spec inner_points([{integer(), integer()}]) :: [{integer(), integer()}]
   def inner_points(points) do
     min_x =
       points
@@ -41,6 +38,49 @@ defmodule Day6 do
       {x, y}
     end
   end
+
+  def create_islands(distances_maps, inner_points) do
+    inner_points
+    |> Enum.reduce(Map.new(), fn {x, y}, map ->
+      case find_nearest_point({x, y}, distances_maps) do
+        nil -> Map.put(map, {x, y}, {-1, -1})
+        {x_nearest, y_nearest} -> Map.put(map, {x, y}, {x_nearest, y_nearest})
+      end
+    end)
+  end
+
+  # {0, 1} => %{{0, 1} => 0, {3, 5} => 7},
+
+  def find_nearest_point({x, y} = _point, distances_maps) do
+    [{{x_nearest, y_nearest}, distance1} | [{{_x2, _y2}, distance2} | _rest]] =
+      distances_maps
+      |> Enum.map(fn {key, distance_map} ->
+        {key, distance_map[{x, y}]}
+      end)
+      |> Enum.sort_by(fn {_key, value} -> value end)
+
+    if distance1 == distance2,
+      do: nil,
+      else: {x_nearest, y_nearest}
+  end
+
+  def create_distances_maps(points, inner_points) do
+    points
+    |> Enum.reduce(Map.new(), fn {x, y}, map ->
+      distances = create_distances_map({x, y}, inner_points)
+      Map.put(map, {x, y}, distances)
+    end)
+  end
+
+  def create_distances_map({x, y} = _point, inner_points) do
+    inner_points
+    |> Enum.reduce(Map.new(), fn {xi, yi}, map ->
+      distance = manhattan_distance({x, y}, {xi, yi})
+      Map.put(map, {xi, yi}, distance)
+    end)
+  end
+
+  def manhattan_distance({x1, y1}, {x2, y2}), do: abs(x1 - x2) + abs(y1 - y2)
 
   def solve2(filename) do
     filename
--- day6/test/day6_test.exs
+++ day6/test/day6_test.exs
@@ -17,6 +17,60 @@ defmodule Day6Test do
     assert actual == [{1, 2}, {1, 3}, {1, 4}, {2, 2}, {2, 3}, {2, 4}, {3, 2}, {3, 3}, {3, 4}]
   end
 
+  test "manhattan_distance" do
+    actual = Day6.manhattan_distance({3, 2}, {1, 5})
+
+    assert actual == 5
+  end
+
+  test "create_distances_map" do
+    actual = Day6.create_distances_map({1, 2}, [{3, 5}, {0, 1}])
+
+    assert actual == %{{0, 1} => 2, {3, 5} => 5}
+  end
+
+  test "create_distances_maps" do
+    actual = Day6.create_distances_maps([{1, 0}, {0, 1}], [{3, 5}, {0, 1}])
+
+    assert actual == %{
+             {0, 1} => %{{0, 1} => 0, {3, 5} => 7},
+             {1, 0} => %{{0, 1} => 2, {3, 5} => 7}
+           }
+  end
+
+  test "find_nearest_point" do
+    points = [{0, 0}, {0, 4}]
+    inner_points = Day6.inner_points(points)
+    distances_maps = Day6.create_distances_maps(points, inner_points)
+
+    actual = Day6.find_nearest_point({0, 1}, distances_maps)
+    assert actual == {0, 0}
+
+    actual = Day6.find_nearest_point({0, 2}, distances_maps)
+    assert actual == nil
+  end
+
+  test "create_islands" do
+    # points = [{1, 1}, {1, 6}, {8, 3}, {3, 4}, {5, 5}, {8, 9}]
+    points = [{0, 0}, {0, 4}]
+    inner_points = Day6.inner_points(points)
+    distances_maps = Day6.create_distances_maps(points, inner_points)
+
+    actual =
+      Day6.create_islands(
+        distances_maps,
+        inner_points
+      )
+
+    assert actual == %{
+             {0, 0} => {0, 0},
+             {0, 1} => {0, 0},
+             {0, 2} => {-1, -1},
+             {0, 3} => {0, 4},
+             {0, 4} => {0, 4}
+           }
+  end
+
   test "sample1" do
     assert Day6.solve1("sample1") == 10
   end
