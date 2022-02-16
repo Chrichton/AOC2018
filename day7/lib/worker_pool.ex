67f074b276e2b25da7a2c3910bfbf7aadbd04e0c
Heiko Goes <heiko.goes@gaebler-goes.de>
2022-02-16 16:47:56 +0100

    workerpool + tests

--- day7/lib/worker_pool.ex
+++ day7/lib/worker_pool.ex
@@ -2,11 +2,11 @@ defmodule WorkerPool do
   defmodule Worker do
     alias __MODULE__, as: Worker
 
-    @type t :: %__MODULE__{char: integer, seconds_to_complete: integer}
+    @type t :: %__MODULE__{char: integer(), seconds_to_complete: pos_integer()}
     @enforce_keys [:char, :seconds_to_complete]
     defstruct [:char, :seconds_to_complete]
 
-    def new(char, seconds) do
+    def new(char, seconds) when is_integer(seconds) do
       %Worker{char: char, seconds_to_complete: seconds}
     end
   end
@@ -32,6 +32,22 @@ defmodule WorkerPool do
       }),
       do: max_worker_count - Enum.count(workers)
 
-  def next_step(%WorkerPool{workers: workers}) do
+  def next_step(%WorkerPool{workers: workers} = worker_pool) do
+    workers
+    |> Enum.reduce(
+      {[], []},
+      fn %Worker{char: char, seconds_to_complete: seconds},
+         {completed_chars, remaining_workers} ->
+        if seconds == 1 do
+          {[char | completed_chars], remaining_workers}
+        else
+          worker = %Worker{char: char, seconds_to_complete: seconds - 1}
+          {completed_chars, [worker | remaining_workers]}
+        end
+      end
+    )
+    |> then(fn {chars, workers} ->
+      {chars, %{worker_pool | workers: workers}}
+    end)
   end
 end
--- day7/test/worker_pool_test.exs
+++ day7/test/worker_pool_test.exs
@@ -22,9 +22,24 @@ defmodule WorkerPoolTest do
   end
 
   test "available_workers" do
-    actual =
+    worker_pool =
       WorkerPool.new(5)
       |> WorkerPool.add_worker(Worker.new(?A, 60))
+
+    assert WorkerPool.available_workers(worker_pool) == 4
+  end
+
+  test "next_step" do
+    worker_pool =
+      WorkerPool.new(5)
+      |> WorkerPool.add_worker(Worker.new(?B, 2))
+      |> WorkerPool.add_worker(Worker.new(?A, 1))
+
+    {[char], actual} = WorkerPool.next_step(worker_pool)
+
+    assert char == ?A
+    assert actual.max_worker_count == 5
+    assert actual.workers == [%Worker{char: ?B, seconds_to_complete: 1}]
 
     assert WorkerPool.available_workers(actual) == 4
   end
