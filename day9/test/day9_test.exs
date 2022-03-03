e2ed4963ddc2adf37914b02b2dfda7ec1a7d500d
Heiko Goes <heiko.goes@gaebler-goes.de>
2022-03-03 17:40:11 +0100

    show error explicit in test

--- day9/test/day9_test.exs
+++ day9/test/day9_test.exs
@@ -52,10 +52,13 @@ defmodule Day9Test do
     assert Day9.winning_score({17, 1104}) == 2764
     assert Day9.winning_score({21, 6111}) == 54718
     assert Day9.winning_score({30, 5807}) == 37305
+
+    # 202 too little
+    assert Day9.winning_score({13, 7999}) == 146_373
   end
 
   test "sample1" do
-    assert Day9.solve1("sample") == 146_373
+    assert Day9.solve1("sample") == 8317
   end
 
   @tag :skip
