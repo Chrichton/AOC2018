0b5cd4419bbebe7a21b0ca025095fa0d1d5c52b9
Heiko Goes <heiko.goes@gaebler-goes.de>
2022-03-15 07:15:54 +0100

    initial

---
+++ day11/lib/day11.ex
@@ -0,0 +1,4 @@
+defmodule Day11 do
+  def solve1(grid_serial_number) do
+  end
+end
---
+++ day11/test/day11_test.exs
@@ -0,0 +1,8 @@
+defmodule Day11Test do
+  use ExUnit.Case
+
+  @tag :skip
+  test "solve1" do
+    assert Day11.solve1(5468) == nil
+  end
+end
