81859b3fd5a8b73237fccbfc152d1c1fea1f3f02
Heiko Goes <heiko.goes@gaebler-goes.de>
2022-03-15 07:36:35 +0100

    extract_hundreds_digit

--- day11/lib/day11.ex
+++ day11/lib/day11.ex
@@ -1,4 +1,25 @@
 defmodule Day11 do
   def solve1(grid_serial_number) do
   end
+
+  def calc_power_level({x, y} = _cell_coordinate, grid_serial_number) do
+    rack_id = x + 10
+    power_level = rack_id * y
+    power_level = power_level + grid_serial_number
+    power_level = power_level * rack_id
+    hundreds_digit = extract_hundreds_digit(power_level)
+    power_level - hundreds_digit
+  end
+
+  @spec extract_hundreds_digit(integer) :: integer
+  def extract_hundreds_digit(number) do
+    digits =
+      number
+      |> Integer.digits()
+      |> Enum.reverse()
+
+    if Enum.count(digits) < 3,
+      do: 0,
+      else: Enum.at(digits, 2)
+  end
 end
--- day11/test/day11_test.exs
+++ day11/test/day11_test.exs
@@ -1,6 +1,13 @@
 defmodule Day11Test do
   use ExUnit.Case
 
+  test "extract_hundreds_digit" do
+    assert Day11.extract_hundreds_digit(0) == 0
+    assert Day11.extract_hundreds_digit(1023) == 0
+    assert Day11.extract_hundreds_digit(123) == 1
+    assert Day11.extract_hundreds_digit(1234) == 2
+  end
+
   @tag :skip
   test "solve1" do
     assert Day11.solve1(5468) == nil
