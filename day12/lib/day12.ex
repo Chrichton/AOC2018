96d724e53e0cc7d87db1b32ae3f56f516b143385
Heiko Goes <heiko.goes@gaebler-goes.de>
2022-03-26 11:55:24 +0100

    parsing done

--- day12/lib/day12.ex
+++ day12/lib/day12.ex
@@ -1,4 +1,19 @@
 defmodule Day12 do
+  defmodule Rule do
+    defstruct [:prev_prev, :prev, :current, :next, :next_next, :result]
+
+    def new([prev_prev, prev, current, next, next_next], result) do
+      %Rule{
+        prev_prev: prev_prev,
+        prev: prev,
+        current: current,
+        next: next,
+        next_next: next_next,
+        result: result
+      }
+    end
+  end
+
   def solve1(filename) do
     filename
     |> read_input()
@@ -10,12 +25,52 @@ defmodule Day12 do
     |> parse_input()
   end
 
-  def parse_input([initial_state | rules]) do
+  def parse_input([initial_state_line | rule_lines]) do
+    start_index = index_of(initial_state_line, ":") + 2
+
     pots =
-      initial_state
+      initial_state_line
+      |> String.slice(start_index, String.length(initial_state_line))
       |> String.codepoints()
       |> Enum.with_index()
       |> Enum.filter(fn {element, _index} -> element == "#" end)
       |> MapSet.new(fn {_element, index} -> index end)
+
+    rules =
+      rule_lines
+      |> Enum.map(&parse_rule/1)
+
+    {pots, rules}
+  end
+
+  def parse_rule(rule_line) do
+    condition_stop_index = index_of(rule_line, "=") - 1
+
+    conditions =
+      rule_line
+      |> String.slice(0, condition_stop_index)
+      |> String.codepoints()
+      |> Enum.map(fn
+        "." -> false
+        "#" -> true
+      end)
+
+    result_index = index_of(rule_line, ">") + 2
+    result = String.slice(rule_line, result_index, 1) == "#"
+
+    Rule.new(conditions, result)
+  end
+
+  def check_rule(rule, pots, pot) do
+    rule.current == MapSet.member?(pots, pot) &&
+      rule.prev == MapSet.member?(pots, pot - 1) &&
+      rule.prev_prev == MapSet.member?(pots, pot - 2) &&
+      rule.next == MapSet.member?(pots, pot + 1) &&
+      rule.next_next == MapSet.member?(pots, pot + 2)
+  end
+
+  def index_of(string, substring) do
+    :binary.match(string, substring)
+    |> elem(0)
   end
 end
--- day12/test/day12_test.exs
+++ day12/test/day12_test.exs
@@ -1,16 +1,27 @@
 defmodule Day12Test do
   use ExUnit.Case
 
-  test "parse_input" do
-    actual = Day12.parse_input(["#..#.#..##......###...###"])
+  test "parse_rule" do
+    actual = Day12.parse_rule("...## => #")
 
-    assert actual == MapSet.new([0, 3, 5, 8, 9, 16, 17, 18, 22, 23, 24])
+    assert actual == %Day12.Rule{
+             prev_prev: false,
+             prev: false,
+             current: false,
+             next: true,
+             next_next: true,
+             result: true
+           }
   end
 
   test "read_input" do
+    {pots, rules} = Day12.read_input("sample")
+
+    assert pots == MapSet.new([0, 3, 5, 8, 9, 16, 17, 18, 22, 23, 24])
+    assert Enum.count(rules) == 14
   end
 
-  test "sample1" do
+  test "sample" do
   end
 
   test "star" do
